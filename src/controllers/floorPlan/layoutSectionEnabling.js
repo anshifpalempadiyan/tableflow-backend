b2bc132bafac4bb11606da8d4278a7020dcbcf8e
anshif-8590 <anshifp949@gmail.com>
2026-04-24 19:05:24 +0530

    created the layout enabling API

--- src/controllers/floorPlan/layoutSectionEnabling.js
+++ src/controllers/floorPlan/layoutSectionEnabling.js
@@ -2,21 +2,19 @@ import TableLayout from "../../models/tableLayout.js"
 
 
 
-
-
-const layoutSectionDisabled = async (req, res) => {
+const layoutSectionEnabling = async (req, res) => {
     try {
-        // console.log(req.body,"the body ")
-        const { id, layoutDisabled } = req.body
+        const { id, layoutenabled } = req.body
 
         const findUser = await TableLayout.findOne({ userId: id })
 
         if (findUser) {
-            const disableingSection = await TableLayout.updateOne({ $addToSet: { disabledSections: layoutDisabled } })
-            return res.status(400).json({ status: true, msg: "Table disabled", disableingSection })
+            const enablingSection = await TableLayout.updateOne({ $pull : { disabledSections : { $in : layoutenabled } }})
+            return res.status(400).json({ status : true , msg : "Table enabled" , enablingSection })
 
         } else {
             return res.status(404).json({ status: false, msg: "User not found" })
+
         }
 
     } catch (error) {
@@ -24,8 +22,6 @@ const layoutSectionDisabled = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" })
     }
 
-
 }
 
-
-export default layoutSectionDisabled
+export default layoutSectionEnabling
--- src/routes/floorPlan/index.js
+++ src/routes/floorPlan/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import layoutCreation from '../../controllers/floorPlan/layoutCreation.js'
 import layoutSectionDisabled from '../../controllers/floorPlan/layoutSectionDisabled.js'
+import layoutSectionEnabling from '../../controllers/floorPlan/layoutSectionEnabling.js'
 
 
 const router = express.Router()
 
 router.post('/create' , layoutCreation )
 router.patch('/disable', layoutSectionDisabled )
+router.patch('/enable' , layoutSectionEnabling )
 
 
 export default router
